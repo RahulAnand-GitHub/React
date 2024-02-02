b2f4b3b3ef8de21c3693638e54f93983ea801739
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2024-02-02 15:16:38 +0530

    Day-1: Inception

---
+++ Inception/app.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello Form React🙏🏻");
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm Rahul Anand."),
+    React.createElement("h2", {}, "And I'm from Bihar."),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Currently pursuing MCA"),
+    React.createElement("h2", {}, "From CGC, Landran."),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
