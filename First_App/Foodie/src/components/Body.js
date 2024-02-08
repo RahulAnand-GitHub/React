0500fc8e9c0db4c22219ebf9f91f9997eeb74362
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2024-02-08 11:55:48 +0530

    Applied the useState hook to filter the restaurant list.

--- First_App/Foodie/src/components/Body.js
+++ First_App/Foodie/src/components/Body.js
@@ -1,14 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-
-
+import { useState } from "react";
 
 const Body = () => {
+  // Local State variable - super powerful variable.
+  const [listOfVariables, setListOfVariables] = useState(resList)
   return (
     <div className="body">
-      <div className="search-bar">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdList = listOfVariables.filter(
+              (res) => res.info.avgRating >= 4
+            );
+            setListOfVariables(filterdList);
+          }}
+        >
+          Top Rated Button
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfVariables.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
@@ -16,4 +29,3 @@ const Body = () => {
   );
 };
 export default Body;
-
